24fcb9c87f019f359b8522a68fabc58b77d18f28
Jumi Yoon <jumiyoon@Jumis-MacBook-Pro.local>
2016-07-10 15:58:20 -0700

    Completed Header Level Website Design

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,4 +2,17 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 
- <div id=”mapDiv”>
+$('#header').prepend(HTMLheaderRole.replace('%data%',bio.role));
+$('#header').prepend(HTMLheaderName.replace('%data%',bio.name));
+$('#topContacts').append(HTMLmobile.replace('%data%',bio.contacts.mobile));
+$('#topContacts').append(HTMLemail.replace('%data%',bio.contacts.email));
+$('#topContacts').append(HTMLtwitter.replace('%data%',bio.contacts.twitter));
+$('#topContacts').append(HTMLgithub.replace('%data%',bio.contacts.github));
+$('#topContacts').append(HTMLlocation.replace('%data%',bio.contacts.location));
+$('#header').append(HTMLbioPic.replace('%data%',bio.biopic));
+$('#header').append(HTMLwelcomeMsg.replace('%data%',bio.welcomeMessage));
+$('#header').append(HTMLskillsStart);
+for(var i in bio.skills) {
+//for(var i = 0; i < bio.skills.length; i++) {
+  $('#skills').append(HTMLskills.replace('%data%',bio.skills[i]));
+}
---
+++ js/resumeData.js
@@ -0,0 +1,75 @@
+var bio = {
+  "name": "Jumi Yoon",
+  "role": "Ux Designer",
+  "contacts": {
+    "mobile": "240-361-8324",
+    "email": "design.jumi@gmail.com",
+    "github": "jj3274@github.com",
+    "twitter": "@jj3274",
+    "location": "San Francisco"
+  },
+  "welcomeMessage": "Welcome Jumi Yoon's Website",
+  "skills": ["Photoshop", "Sketch", "Illustrator","HTML,CSS,JavaScript"],
+  "biopic": "images/JumiYoon Profile picture.jpg",
+  "display": "function"
+};
+
+var education = {
+  "schools": [
+    {
+      "name": "SF School",
+      "location": "SF",
+      "degree": "Certificate",
+      "majors": ["UX Design", "Interior Design"],
+      "dates": "2015-06-30",
+      "url": "http://...."
+    },
+    {
+      "name": "Another School",
+      "location": "SF",
+      "degree": "Certificate",
+      "majors": ["UX Design", "Interior Design"],
+      "dates": "2015/06/30-2015/08/30",
+      "url": "http://...."
+    }
+  ],
+  "onlineCourses": [
+    {
+      "title": "string",
+      "school": "string",
+      "date": "2015/06/30-2015/08/30",
+      "url": "string"
+    },
+    {
+      "title": "string",
+      "school": "string",
+      "date": "2015/06/30-2015/08/30",
+      "url": "string"
+    }
+  ],
+  "display": "function"
+};
+
+var work = {
+  "jobs": [
+    {
+      "employer": "string",
+      "title": "string",
+      "location": "string",
+      "dates": "in progress",
+      "description": "string"
+    }
+  ],
+  "display": "function"
+};
+
+var projects = {
+"projects":[
+  {
+    "title": "string",
+    "dates": "2015/06/30-2015/08/30",
+    "description": "string",
+    "images": ["url1", "http://", "http://rrrr"],
+    "display": "function"
+  }
+]};
