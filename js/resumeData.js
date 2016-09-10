1fd7b726b3c2a0cbd3a1125380c3acd1903dd236
Jumi Yoon <design.jumi@gmail.com>
2016-09-10 11:20:55 -0700

    Submitted for online resume

--- js/resumeData.js
+++ js/resumeData.js
@@ -8,68 +8,72 @@ var bio = {
     "twitter": "@jj3274",
     "location": "San Francisco"
   },
-  "welcomeMessage": "Welcome Jumi Yoon's Website",
-  "skills": ["Photoshop", "Sketch", "Illustrator","HTML,CSS,JavaScript"],
+  "welcomeMessage": "Welcome to Jumi Yoon's Website",
+  "skills": ["Photoshop", "Sketch", "Illustrator", "HTML,CSS,JavaScript"],
   "biopic": "images/JumiYoon Profile picture.jpg",
   "display": "function"
 };
 
 var education = {
-  "schools": [
-    {
-      "name": "SF School",
-      "location": "SF",
-      "degree": "Certificate",
-      "majors": ["UX Design", "Interior Design"],
-      "dates": "2015-06-30",
-      "url": "http://...."
-    },
-    {
-      "name": "Another School",
-      "location": "SF",
-      "degree": "Certificate",
-      "majors": ["UX Design", "Interior Design"],
-      "dates": "2015/06/30-2015/08/30",
-      "url": "http://...."
-    }
-  ],
-  "onlineCourses": [
-    {
-      "title": "string",
-      "school": "string",
-      "date": "2015/06/30-2015/08/30",
-      "url": "string"
-    },
-    {
-      "title": "string",
-      "school": "string",
-      "date": "2015/06/30-2015/08/30",
-      "url": "string"
-    }
-  ],
+  "schools": [{
+    "name": "San francisco State University Extension",
+    "location": "San francisco,CA, USA",
+    "degree": "User Experience Design Certificate",
+    "majors": ["UX Design"],
+    "dates": "2015/10-2015/11",
+    "url": "http://...."
+  }, {
+    "name": "Duksung Women's University",
+    "location": "Seoul, South Korea",
+    "degree": "Bachelor of Fine Art",
+    "majors": ["Interior Design"],
+    "dates": "2000/03-2004/03",
+    "url": "http://...."
+  }],
+  "onlineCourses": [{
+    "title": "Front-End Web Developer Nanodegree",
+    "school": "Udacity",
+    "dates": "2015/07-2016/08",
+    "url": "http://udacity.com"
+  }, ],
   "display": "function"
 };
 
 var work = {
-  "jobs": [
-    {
-      "employer": "string",
-      "title": "string",
-      "location": "string",
-      "dates": "in progress",
-      "description": "string"
-    }
-  ],
+  "jobs": [{
+    "employer": "Emento.Inc",
+    "title": "UX Designer",
+    "location": "Foster City, California, USA",
+    "dates": "2015.07 - Present",
+    "description": "Executing design work consistent with design and brand standards, analytics insight, customer feedback"
+  }, {
+    "employer": "Self-employee",
+    "title": "UX Designer",
+    "location": "Foster City, California, USA",
+    "dates": "2015.01 - 2015.06",
+    "description": "Sketching, concept creation and wire framing, producing sitemaps and user flows, creating clickable prototypes. Working with user research teams to plan usability testing sessions and delivers artifacts for testing. Using user insight, analytics, best practice, competitor analysis to inform design decisions."
+  }, {
+    "employer": "Rise Architecture",
+    "title": "Interior Designer",
+    "location": "Bethesda, Maryland, USA",
+    "dates": "2006.03 - 2008.05",
+    "description": "Responsible for designing spaces inside buildings. Selects color schemes, window treatments, hardware and lighting fixtures, carpet, artwork, paint, and furniture. Works with architect on layout of rooms."
+  }],
   "display": "function"
 };
 
 var projects = {
-"projects":[
-  {
-    "title": "string",
-    "dates": "2015/06/30-2015/08/30",
-    "description": "string",
-    "images": ["url1", "http://", "http://rrrr"],
+  "projects": [{
+    "title": "Emento app",
+    "dates": "2015.07-present",
+    "description": "Social media app for sharing pictures, videos through digital signage at special events",
+    "images": ["images/Emento1.png", "images/Emento2.png", "images/Emento3.png"],
     "display": "function"
-  }
-]};
+  }, {
+    "title": "Outfit builder App",
+    "dates": "2015.11-2015.12",
+    "description": "App for make personal outfit builder through websearch and social network",
+    "images": ["images/outfit1.png", "images/outfit2.png", "images/outfit3.png"],
+    "display": "function"
+  }]
+};
